1dd25507770ac6cb036691f39dec68fa19222fa4
kaiwenr <kaiwenr@kth.se>
2024-02-27 22:30:47 +0100

    完成商户/菜单查阅，未测试添加菜单,allowed-hosts=*

--- Project2216/settings.py
+++ Project2216/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tucyibw+n9$=v0rclm-xh+h8wsn^7f8t!&q$f*nwuez=8lag6=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
