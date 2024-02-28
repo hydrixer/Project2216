8e3e1be567058e71e5e65da5f7b6fab7b07fbab8
kaiwenr <kaiwenr@kth.se>
2024-02-28 23:58:00 +0100

    debug usage

--- iorder/views.py
+++ iorder/views.py
@@ -274,6 +274,7 @@ def owner_menu(request):
     else:
         username = request.query_params.get('username')
     try:
+        print(username)
         shop = Shop.objects.get(host=User.objects.get(username=username))
         shop_dish = Dish.objects.filter(shop_index=shop.shop_index)
         list = []
